1d6b232d38d9a15d2fe9295281142fa4c09debd6
Termpey <cjt032000@gmail.com>
2018-05-18 09:20:53 -0500

    Working hold

--- src/components/organism/GameManager.js
+++ src/components/organism/GameManager.js
@@ -47,46 +47,19 @@ export default class GameManager {
 
     hold(data) {
 
-        let diceValues = this.state.diceValues.slice();
-        let scoreList = []
-        let count = 0
-        let number = this.state.numberHeld
-
-        if(number === 0 || number === diceValues[data].value || diceValues[data].value === 1){
-            
-            for (let i in diceValues) {
-                if (diceValues[i].value === 1) {
-                    count = count + 1
-                }
-            }
-    
-            if (count >= 1) {
-    
-                if(diceValues[data].value === 1){
-                    diceValues[data].held = !diceValues[data].held;
-                }else{
-                    if (number.isHeld === true){
-                        if(diceValues[data].value === number.value){
-                            diceValues[data].held = !diceValues[data].held;
-                        }
-                    }else{
-                        diceValues[data].held = !diceValues[data].held;
-                        
-                    }
-                }
-    
-                for (let i in diceValues) {
-                    if (diceValues[i].held === true) {
-                        scoreList.push(diceValues[i].value)
-                    }
-                }
-            }
-        }
-
-        this.state.score = Score(this.state.rolls, scoreList)
-        this.state.diceValues = diceValues
-
-        return clone(this.state)
+        return new Promise(function(resolve, reject){
+            fetch('http://localhost:8080/hold' + data, {
+                credentials: 'include'
+            }).then(function(response){
+                console.log(response.body);
+                return response.json();
+            }).then(function(json){
+                resolve(json);
+            }).catch(function(err){
+                reject(err);
+            });
+        });
+        
     }
 
     roll() {
